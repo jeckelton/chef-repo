28fa3d233f82336722e199f4bfa119c0db817e6c
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2025-04-03 14:19:08 +0200

    Updating Graylog Cookbook

--- cookbooks/graylog2/attributes/default.rb
+++ cookbooks/graylog2/attributes/default.rb
@@ -3,6 +3,6 @@ default['graylog2']['repo_deb_path'] = '/tmp/graylog-5.2-repository_latest.deb'
 
 default['graylog2']['server']['http_bind_address'] = '0.0.0.0'
 
-default['graylog2']['mongodb']['host'] = '0.0.0.0'
-default['graylog2']['elasticsearch']['host'] = '0.0.0.0'
+default['graylog2']['mongodb']['host'] = 'graylog.fritz.box'
+default['graylog2']['elasticsearch']['host'] = 'graylog.fritz.box'
 default['graylog2']['elasticsearch']['cluster_name'] = 'graylog'
