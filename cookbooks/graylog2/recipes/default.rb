c55ac37f96a3cf74cb7900c284760ef4da59ced2
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2025-04-03 19:54:38 +0200

    Updating Graylog Cookbook

--- cookbooks/graylog2/recipes/default.rb
+++ cookbooks/graylog2/recipes/default.rb
@@ -4,9 +4,11 @@
 #
 # Copyright:: 2025, The Authors, All Rights Reserved.
 #
+include_recipe '::firewalld'
 include_recipe 'graylog2::java'
 include_recipe 'graylog2::mongodb'
 include_recipe 'graylog2::elasticsearch'
 include_recipe 'graylog2::install'
 include_recipe 'graylog2::configure'
 include_recipe 'graylog2::service'
+
