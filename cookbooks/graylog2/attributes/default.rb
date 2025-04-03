d60cc0b24533469af45a2badd6a8b00bfbca8aef
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2025-04-03 13:59:50 +0200

    Updating Graylog Cookbook

--- cookbooks/graylog2/attributes/default.rb
+++ cookbooks/graylog2/attributes/default.rb
@@ -1,10 +1,8 @@
 default['graylog2']['repo_url'] = 'https://packages.graylog2.org/repo/packages/graylog-5.2-repository_latest.deb'
 default['graylog2']['repo_deb_path'] = '/tmp/graylog-5.2-repository_latest.deb'
 
-default['graylog2']['server']['password_secret'] = secrets['password_secret']
-default['graylog2']['server']['root_password_sha2'] = secrets['root_password_sha2']
 default['graylog2']['server']['http_bind_address'] = '0.0.0.0'
 
-default['graylog2']['mongodb']['host'] = '127.0.0.1'
-default['graylog2']['elasticsearch']['host'] = '127.0.0.1'
+default['graylog2']['mongodb']['host'] = '0.0.0.0'
+default['graylog2']['elasticsearch']['host'] = '0.0.0.0'
 default['graylog2']['elasticsearch']['cluster_name'] = 'graylog'
--- cookbooks/graylog2/recipes/configure.rb
+++ cookbooks/graylog2/recipes/configure.rb
@@ -1,5 +1,9 @@
 secrets = data_bag_item('graylog2', 'secrets')
 
+node.override['graylog2']['server']['password_secret'] = secrets['password_secret']
+node.override['graylog2']['server']['root_password_sha2'] = secrets['root_password_sha2']
+
+
 template '/etc/graylog/server/server.conf' do
   source 'server.conf.erb'
   owner 'graylog'
@@ -11,6 +15,7 @@ template '/etc/graylog/server/server.conf' do
     mongodb_host: node['graylog2']['mongodb']['host'],
     elasticsearch_host: node['graylog2']['elasticsearch']['host'],
     elasticsearch_cluster_name: node['graylog2']['elasticsearch']['cluster_name']
+    http_bind_address: node['graylog2']['server']['http_bind_address']
   )
   notifies :restart, 'service[graylog-server]'
 end
