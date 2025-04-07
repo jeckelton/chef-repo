1e5bcfc0d4e8fd17e02a0fbf4d93ca0b90cd55f6
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2025-04-07 11:56:24 +0200

    Updating Graylog Cookbook

---
+++ cookbooks/graylog2/recipes/repo_configure.rb
@@ -0,0 +1,18 @@
+repo_url = node['graylog2']['repo_url']
+repo_deb_path = node['graylog2']['repo_deb_path']
+
+remote_file repo_deb_path do
+  source repo_url
+  action :create
+  not_if { ::File.exist?(repo_deb_path) }
+end
+
+dpkg_package 'graylog-repository' do
+  source repo_deb_path
+  action :install
+end
+
+execute 'apt_update' do
+  command 'apt-get update'
+  only_if { ::File.mtime('/var/lib/apt/lists') < Time.now - 86_400 }
+end
--- cookbooks/graylog2/recipes/server_install.rb
+++ cookbooks/graylog2/recipes/server_install.rb
@@ -1,21 +1,4 @@
-repo_url = node['graylog2']['repo_url']
-repo_deb_path = node['graylog2']['repo_deb_path']
-
-remote_file repo_deb_path do
-  source repo_url
-  action :create
-  not_if { ::File.exist?(repo_deb_path) }
-end
-
-dpkg_package 'graylog-repository' do
-  source repo_deb_path
-  action :install
-end
-
-execute 'apt_update' do
-  command 'apt-get update'
-  only_if { ::File.mtime('/var/lib/apt/lists') < Time.now - 86_400 }
-end
+include_recipe 'graylog2::repo_configure'
 
 package 'graylog-server' do
   action :install
