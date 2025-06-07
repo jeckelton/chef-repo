c44c9c0efd74553ee0145767e3abe31240154542
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2025-06-07 21:29:12 +0200

    Adding new rundeck cookbook

---
+++ cookbooks/rundeck/attributes/default.rb
@@ -0,0 +1,27 @@
+# Yum Repo
+default['rundeck']['repo']['description'] = 'rundeck'
+default['rundeck']['repo']['baseurl'] = 'https://packages.rundeck.com/pagerduty/rundeck/rpm_any/rpm_any/$basearch'
+default['rundeck']['repo']['repo_gpgcheck'] = true
+default['rundeck']['repo']['gpgcheck'] = false
+default['rundeck']['repo']['enabled'] = true
+default['rundeck']['repo']['gpgkey'] = 'https://packages.rundeck.com/pagerduty/rundeck/gpgkey'
+default['rundeck']['repo']['sslverify'] = true
+default['rundeck']['repo']['sslcacert'] = '/etc/pki/tls/certs/ca-bundle.crt'
+default['rundeck']['repo']['metadata_expire'] = '300'
+default['rundeck']['version'] = '5.10.1.20250415-1'
+
+# Framework config
+default['rundeck']['framework']['hostname'] = 'http://testvm.fritz.box'
+default['rundeck']['framework']['name'] = 'Rundeck'
+default['rundeck']['framework']['port'] = '4440'
+default['rundeck']['framework']['server_url'] = 'http://testvm.fritz.box:4440'
+
+# DB settings
+default['rundeck']['db']['name'] = 'rundeck'
+default['rundeck']['db']['user'] = 'rundeck'
+default['rundeck']['db']['password'] = 'TheSunSets2025!'
+
+# MariaDB Root Password
+default['rundeck']['db']['root_pw'] = '7ck^jVwwYfVE&@wQ'
+
+
