94f01c33900a6ea830b11c006008422686692a51
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2025-06-07 21:31:01 +0200

    Adding new rundeck cookbook

--- cookbooks/rundeck/metadata.rb
+++ cookbooks/rundeck/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Jeremy Eckelton'
 maintainer_email 'jeremy.eckelton@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures rundeck'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 16.0'
 
 supports 'rocky'
