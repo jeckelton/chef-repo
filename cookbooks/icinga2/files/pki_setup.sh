c7b1fe40620c1a610fa070742dbe331a19e7b76b
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2025-06-19 12:28:16 +0200

    Updating Icinga2 cookbook

--- cookbooks/icinga2/files/pki_setup.sh
+++ cookbooks/icinga2/files/pki_setup.sh
@@ -3,8 +3,8 @@ set -e
 
 CN="$1"
 
-mkdir -p /var/lib/icinga2/certs /var/lib/icinga2/ca
-chown -R nagios:nagios /var/lib/icinga2/certs /var/lib/icinga2/ca
+mkdir -p /var/lib/icinga2/certs
+chown -R nagios:nagios /var/lib/icinga2/certs
 
 if [ ! -f "/var/lib/icinga2/certs/${CN}.crt" ]; then
   icinga2 pki new-cert \
@@ -15,7 +15,7 @@ if [ ! -f "/var/lib/icinga2/certs/${CN}.crt" ]; then
   icinga2 pki save-cert \
     --key "/var/lib/icinga2/certs/${CN}.key" \
     --cert "/var/lib/icinga2/certs/${CN}.crt" \
-    --trustedcert "/var/lib/icinga2/ca/ca.crt" \
+    --trustedcert "/var/lib/icinga2/certs/ca.crt" \
     --host "$CN"
 fi
 
