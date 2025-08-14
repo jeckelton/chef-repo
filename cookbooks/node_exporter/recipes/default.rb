503e716ab7401b2b4972b6781d2ec44756207285
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2025-08-14 10:19:45 +0200

    Updating node_exporter service

--- cookbooks/node_exporter/recipes/default.rb
+++ cookbooks/node_exporter/recipes/default.rb
@@ -84,8 +84,8 @@ file '/etc/node_exporter/web.yml' do
     tls_server_config:
       cert_file: "#{node['node_exporter']['ssl_node_cert']}"
       key_file: "#{node['node_exporter']['ssl_node_key']}"
-      client_auth_type: "RequireAndVerifyClientCert"
-      client_ca_file: "#{node['node_exporter']['ssl_ca_cert']}"
+      #client_auth_type: "RequireAndVerifyClientCert" - Uncomment to enable mTLS with Prometheus
+      #client_ca_file: "#{node['node_exporter']['ssl_ca_cert']}" - Uncomment to enable mTLS with Prometheus
   YML
   owner node['node_exporter']['user']
   group node['node_exporter']['group']
