6df89ce0098994f62d3d3ac1b0178e7eb2c32706
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2026-06-06 20:12:02 +0200

    k3s to proxmox

--- cookbooks/nec_k3s/attributes/default.rb
+++ cookbooks/nec_k3s/attributes/default.rb
@@ -5,26 +5,26 @@ default['k3s']['cluster_name'] = 'homelab-k3s'
 default['k3s']['cluster_domain'] = 'cluster.local'
 
 default['k3s']['primary_server'] = 'k3s-cp-01'
-default['k3s']['primary_server_ip'] = '192.168.178.231'
+default['k3s']['primary_server_ip'] = '192.168.178.151'
 
 default['k3s']['api_port'] = 6443
 
 default['k3s']['servers'] = {
-  'k3s-cp-01' => '192.168.178.231',
-  'k3s-cp-02' => '192.168.178.232',
-  'k3s-cp-03' => '192.168.178.233'
+  'k3s-cp-01' => '192.168.178.151',
+  'k3s-cp-02' => '192.168.178.152',
+  'k3s-cp-03' => '192.168.178.153'
 }
 
 default['k3s']['agents'] = {
-  'k3s-worker-01' => '192.168.178.234',
-  'k3s-worker-02' => '192.168.178.235',
-  'k3s-worker-03' => '192.168.178.236'
+  'k3s-worker-01' => '192.168.178.154',
+  'k3s-worker-02' => '192.168.178.155',
+  'k3s-worker-03' => '192.168.178.156'
 }
 
 default['k3s']['tls_sans'] = [
-  '192.168.178.231',
-  '192.168.178.232',
-  '192.168.178.233',
+  '192.168.178.151',
+  '192.168.178.152',
+  '192.168.178.153',
   'k3s-cp-01',
   'k3s-cp-02',
   'k3s-cp-03'
