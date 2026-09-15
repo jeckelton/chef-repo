8d27e3f10497f4cd3ad1460d6679e6d3983187b9
Jeremy Eckelton <jeremy.eckelton@newcold.com>
2026-09-15 18:53:53 +0200

    Time:
    Add Argo CD ingress

--- cookbooks/nec_rke2/recipes/manifests.rb
+++ cookbooks/nec_rke2/recipes/manifests.rb
@@ -4,14 +4,24 @@
 #
 # RKE2 server manifests are cluster-wide AddOns. We manage them only on the
 # bootstrap node to keep one authoritative copy.
+#
 
 node_ip = node['rke2']['node_ip'] || node['ipaddress']
 bootstrap = node_ip == node['rke2']['bootstrap_node_ip']
+
+Chef::Log.info(
+  "RKE2 manifest management: node_ip=#{node_ip}, " \
+  "bootstrap_node_ip=#{node['rke2']['bootstrap_node_ip']}, " \
+  "bootstrap=#{bootstrap}"
+)
 
 return unless bootstrap
 
 manifest_dir = node['rke2']['manifest_dir']
 
+#
+# RKE2 manifest directory
+#
 directory manifest_dir do
   owner 'root'
   group 'root'
@@ -19,6 +29,9 @@ directory manifest_dir do
   recursive true
 end
 
+#
+# Cilium configuration
+#
 template "#{manifest_dir}/rke2-cilium-config.yaml" do
   source 'rke2-cilium-config.yaml.erb'
   owner 'root'
@@ -31,6 +44,9 @@ template "#{manifest_dir}/rke2-cilium-config.yaml" do
   )
 end
 
+#
+# kube-vip RBAC
+#
 template "#{manifest_dir}/kube-vip-rbac.yaml" do
   source 'kube-vip-rbac.yaml.erb'
   owner 'root'
@@ -38,6 +54,9 @@ template "#{manifest_dir}/kube-vip-rbac.yaml" do
   mode '0644'
 end
 
+#
+# kube-vip control-plane VIP
+#
 template "#{manifest_dir}/kube-vip.yaml" do
   source 'kube-vip.yaml.erb'
   owner 'root'
@@ -51,10 +70,15 @@ template "#{manifest_dir}/kube-vip.yaml" do
   )
 end
 
-template "#{node['rke2']['manifest_dir']}/rke2-traefik-config.yaml" do
+#
+# RKE2 Traefik configuration
+#
+# Configure the RKE2-managed Traefik service as a LoadBalancer.
+# Cilium LB IPAM/L2 announcements will provide the external IP.
+#
+template "#{manifest_dir}/rke2-traefik-config.yaml" do
   source 'rke2-traefik-config.yaml.erb'
   owner 'root'
   group 'root'
   mode '0644'
-  only_if { node['rke2']['node_ip'] == node['rke2']['bootstrap_node_ip'] }
 end
